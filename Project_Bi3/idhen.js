b9e2503f98db06f015a0cae92fdf46f3bd17e535
Achintya08 <49245068+Achintya08@users.noreply.github.com>
2021-02-13 22:17:27 +0530

    final update

--- Project_Bi3/idhen.js
+++ Project_Bi3/idhen.js
@@ -1,13 +1,25 @@
-
+var x = document.getElementById("divOut");
+x.style.display="none";
+    
     function userNameInput () {
+
         var Name = document.getElementById('name').value;
         var college = document.getElementById('college').value;
         var location = document.getElementById('location').value;
+        if(!Name && !college && !location){
+            alert("Enter details");
+        }
+        else{
+
         document.getElementById('namess').innerHTML="Name : "+Name;
         document.getElementById('collegess').innerHTML="Location : "+location;
         document.getElementById('locationss').innerHTML="college : "+college;
+        x.style.display="block";}
     }
-    
+    var loadFile = function(event) {
+        var image = document.getElementById('blah');
+        image.src = URL.createObjectURL(event.target.files[0]);
+    };
     
     submitButton.addEventListener('click', userNameInput, false);
    
